7b99577cfa071e8b82a71d1342db69b55e2e2cfd
Bjorn <bjorn@sumocoders.be>
2016-09-29 16:13:40 +0200

    Slugify name before testing

--- src/Tests/StylesheetExporterTest.php
+++ src/Tests/StylesheetExporterTest.php
@@ -30,12 +30,13 @@ class StylesheetExporterTest extends BaseTest
         StylesheetExporter::save($stacker, $this->storagePath);
 
         $packedImage = $this->storagePath.'/'.$stacker->getName().'.png';
-        $packedCSS = $this->storagePath.'/'.$stacker->getName().'.css';
+        $packedCSS = $this->storagePath.'/'.$this->slugify($stacker->getName()).'.css';
 
-        $this->assertTrue(file_exists($packedImage));
-        $this->assertTrue(file_exists($packedCSS));
+        $this->assertTrue(file_exists($packedImage), $packedImage . " doesn't exist");
+        $this->assertTrue(file_exists($packedCSS), $packedCSS . " doesn't exist");
 
-        $css = '.unittest{display:block;background-image:url("' . $packedImage . '");}.unittest-bjorvack{width:180px;height:180px;background-position:0px0px;}';
+        $css = '.unittest{display:block;background-image:url("' . $packedImage . '");}';
+        $css .= '.unittest-bjorvack{width:180px;height:180px;background-position:0px0px;}';
 
         $this->assertEquals(
             $this->simplyfyString(file_get_contents($packedCSS)),
@@ -49,5 +50,21 @@ class StylesheetExporterTest extends BaseTest
     private function simplyfyString($text)
     {
         return str_replace("\r", '', str_replace("\n", '', str_replace("\t", '', str_replace(' ', '', $text))));
+    }
+
+    private function slugify($string)
+    {
+        $string = utf8_encode($string);
+        $string = iconv('UTF-8', 'ASCII//TRANSLIT', $string);
+        $string = preg_replace('/[^a-z0-9- ]/i', '', $string);
+        $string = str_replace(' ', '-', $string);
+        $string = trim($string, '-');
+        $string = strtolower($string);
+
+        if (empty($string)) {
+            return 'n-a';
+        }
+
+        return $string;
     }
 }
